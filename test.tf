1d8f2c30cc3e90576346b59a67499c1dfe07a64b
marvinmoovit <marvin.alfaro@mooviit.com>
2025-02-03 13:44:23 -0600

    Add files via upload

---
+++ test.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    meraki = {
+      source = "cisco-open/meraki"
+      version = "1.0.0-beta"
+    }
+  }
+}
+
+provider "meraki" {
+  # Configuration options
+}
